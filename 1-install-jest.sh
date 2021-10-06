f7cb6f57a0ca65ceb15861dff295f0cc793f72ed
Chris N <enclaveFactor@protonmail.com>
2021-10-05 21:05:18 -0400

    init

---
+++ 1-install-jest.sh
@@ -0,0 +1,13 @@
+# testing library
+# https://jestjs.io/docs/getting-started
+npm install --save-dev jest
+
+# Three laws of Test Driven Development
+# Over the years I have come to describe Test Driven Development in terms of three simple rules. They are:
+
+# You are not allowed to write any production code unless it is to make a failing unit test pass.
+# You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+# You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+
+# https://www.youtube.com/watch?v=qkblc5WRn-U
+# http://www.butunclebob.com/ArticleS.UncleBob.TheThreeRulesOfTdd
