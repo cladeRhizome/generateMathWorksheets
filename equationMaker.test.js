1d844ab60ddf5daac8373d1c1f9d13f5d3f4aa87
Chris N <enclaveFactor@protonmail.com>
2021-10-05 22:04:19 -0400

    change in business requirements! They need us to parameterize the equation()

--- equationMaker.test.js
+++ equationMaker.test.js
@@ -3,7 +3,7 @@ const sum = require('./sum');
 const subtract = require('./subtract');
 
 test('We expect subtraction to be correct', () => {
-  let array = equation();
+  let array = equation('subtraction');
   let firstNumber  = array[0];
   let operator     = array[1];
   let secondNumber = array[2];
@@ -15,13 +15,13 @@ test('We expect subtraction to be correct', () => {
 });
 
 test('We expect addition to be correct', () => {
-  let array = equation();
+  let array = equation('addition');
   let firstNumber  = array[0];
   let operator     = array[1];
   let secondNumber = array[2];
   let answer       = array[3];
 
-  if('subtract' === operator) {
+  if('addition' === operator) {
     expect(sum(firstNumber, secondNumber)).toBe(answer);
   }
 });
