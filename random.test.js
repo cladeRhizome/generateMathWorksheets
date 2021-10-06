0008fe19198bc9d44a0bd2ea63b48df9db09b6b1
Chris N <enclaveFactor@protonmail.com>
2021-10-05 21:33:18 -0400

    try to prevent the same number from appearing each time during random()

--- random.test.js
+++ random.test.js
@@ -2,4 +2,8 @@ const random = require('./random');
 
 test('Generate a random number between 0 and 37', () => {
   expect(random(37)).toBeGreaterThan(0);
+  
+  // highly improbable that we get 
+  // the same number twice in a row
+  expect(random(37)).not.toBe(random(37));
 });
